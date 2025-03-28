998c4b86d0bb64c14dd0d729d74747eb26e64f16
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-28 16:28:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// ====================expression js=========================================
+// const name="himanshi"
+// const age=20
+// const salary=34000
+
+// const App=()=>{
+//   return(
+//     <p>welcome to react i am {name} my age is {age} my salary {salary*2}</p>
+//   )
+// }
+
+// export default App;
+// ==================================inserting a large block of element==================================
+// const subject=<ul>
+//   <li>php</li>
+//   <li>java</li>
+//   <li>oracle</li>
+//   <li>sql</li>
+// </ul>
+// const App=()=>{
+//   return(
+//     <h1>my subject :{subject}</h1>
+//   )
+// }
+// export default App;
+
+// =============================one top level element=====================================================
+
+const App=()=>{
+     return(
+      <>
+       <h1>welcome to react</h1>
+       <h1>i am developer</h1>
+      </>
+     )
+   }
+   export default App;
+   
