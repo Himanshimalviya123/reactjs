cc39cfa19b80b411eaef636b905e48b52a46c0cf
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-04-03 20:00:39 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -266,19 +266,19 @@
 // }
 // export default Home;
 // ===========================================================================
-import Data from "./data";
-import Bhopal from "./bhopal";
-const APP=()=>{
-  return(
-      <>
-      <h1 id="btn">this is data file</h1>
-      <Data/>
-      <Bhopal/>
+// import Data from "./data";
+// import Bhopal from "./bhopal";
+// const APP=()=>{
+//   return(
+//       <>
+//       <h1 id="btn">this is data file</h1>
+//       <Data/>
+//       <Bhopal/>
       
-      </>
-  )
-}
-export default APP;
+//       </>
+//   )
+// }
+// export default APP;
 //=======================================================================================================================================
 // import EmpData from "./emp";
 // import Design from "./empdesing";
@@ -305,3 +305,106 @@ export default APP;
 //   )
 // }
 // export default App;
+// =======================================================================
+
+// import myimg from "./images/n1.jpeg";
+// import myimg1 from "./images/n2.jpeg";
+// import myimg2 from "./images/n3.jpeg";
+// const APP=()=>{
+//   return(
+//       <>
+//       <h1>this is data file</h1>
+//       <img src={myimg} width="300" height="300" />
+//       <img src={myimg1} width="300" height="300" />
+//       <img src={myimg2} width="300" height="300" />
+//       </>
+    
+      
+    
+//   )
+// }
+// export default APP;
+// =======================================================
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Carousel from 'react-bootstrap/Carousel';
+// import myimg from "./images/n1.jpeg";
+// import myimg1 from "./images/n2.jpeg";
+// import myimg2 from "./images/n3.jpeg";
+
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
+// const APP=()=>{
+//   return(
+//       <>
+//       <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container>
+//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#home">Home</Nav.Link>
+//             <Nav.Link href="#link">Link</Nav.Link>
+//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.2">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action/3.4">
+//                 Separated link
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+      
+//     <Carousel>
+//       <Carousel.Item>
+//       <img src={myimg} width="300" height="300" />
+//         <Carousel.Caption>
+//           <h3>First slide label</h3>
+//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//       <img src={myimg1} width="300" height="300" />
+//         <Carousel.Caption>
+//           <h3>Second slide label</h3>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item>
+//       <img src={myimg2} width="300" height="300" />
+//         <Carousel.Caption>
+//           <h3>Third slide label</h3>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p>
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+
+
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="holder.js/100px180" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+
+    
+//       </>
+
+//   )
+// }
+// export default APP;
